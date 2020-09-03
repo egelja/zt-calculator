2a13b9efbb6c5746345d6022c2dfcb545f715dfd
AlemSnyder <44094752+AlemSnyder@users.noreply.github.com>
2020-09-03 18:07:23 -0500

    combined csvReader

--- csvReader.js
+++ csvReader.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 function OpenFile(FilePath) {
     window.open(FilePath); // Wow The worst wrapper in history
 }
@@ -20,28 +20,6 @@ function csvReader(CSVText) {
     for (i = 0; i < csvArray.length; i++) {
         csvArray[i] = csvArray[i].split(",");
     }
-=======
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
-}
-
-function OpenInput(csvReader) {
-    var filesInput = document.getElementById("CSVFile").value;
-    var file = filesInput.files.item(0);
-    var reader = new FileReader();
-    reader.onload = function () {
-        csvReader(reader.result);
-    };
-    csvText = reader.readAsText(file);
-}
-
-function csvReader(CSVText) {
-    var csvArray = CSVText.split("\n");
-    var i = 0;
-    for (i = 0; i < csvArray.length; i++) {
-        csvArray[i] = csvArray[i].split(",");
-    }
->>>>>>> ca6677e64c99ffee9280e3c9336afe292054d407
 }
 
 function ChangeContent(params) {
