c7bf9dae974236d8aa9c768340878c4e6be1d77f
AlemSnyder <44094752+AlemSnyder@users.noreply.github.com>
2020-09-03 14:04:20 -0500

    Integrated csvReader With html

--- csvReader.js
+++ csvReader.js
@@ -1,22 +1,26 @@
-function myFunction() {
-  document.getElementById("demo").innerHTML = "Paragraph changed.";
+function OpenFile(FilePath){
+  window.open(FilePath); // Wow The worst wrapper in history
 }
 
-
-function OpenInput(csvReader){
-var filesInput = document.getElementById("CSVFile").value;
-var file = filesInput.files.item(0);
-var reader = new FileReader();
-reader.onload = function() {
+function OpenInput(csvReader, filesInput) {
+  var filesInput = document.getElementById("CSVFile").value;
+  var file = filesInput.files.item(0);
+  var reader = new FileReader();
+  reader.onload = function () {
     csvReader(reader.result);
   }
-csvText = reader.readAsText(file);
+  csvText = reader.readAsText(file);
+  return csvText;
 }
 
-function csvReader(CSVText){
+function csvReader(CSVText) {
   var csvArray = CSVText.split("\n");
-  var i=0;
-  for (i=0;i<csvArray.length; i++){
+  var i = 0;
+  for (i = 0; i < csvArray.length; i++) {
     csvArray[i] = csvArray[i].split(",");
   }
+}
+
+function ChangeContent(params) {
+  document.getElementById("demo").innerHTML = OpenInput(csvReader, OpenInput);
 }
