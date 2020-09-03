ca6677e64c99ffee9280e3c9336afe292054d407
Nino Maruszewski <nino.maruszewski@gmail.com>
2020-09-03 13:59:10 -0500

    Prettier config, formatting.

--- csvReader.js
+++ csvReader.js
@@ -1,22 +1,21 @@
 function myFunction() {
-  document.getElementById("demo").innerHTML = "Paragraph changed.";
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
 
-
-function OpenInput(csvReader){
-var filesInput = document.getElementById("CSVFile").value;
-var file = filesInput.files.item(0);
-var reader = new FileReader();
-reader.onload = function() {
-    csvReader(reader.result);
-  }
-csvText = reader.readAsText(file);
+function OpenInput(csvReader) {
+    var filesInput = document.getElementById("CSVFile").value;
+    var file = filesInput.files.item(0);
+    var reader = new FileReader();
+    reader.onload = function () {
+        csvReader(reader.result);
+    };
+    csvText = reader.readAsText(file);
 }
 
-function csvReader(CSVText){
-  var csvArray = CSVText.split("\n");
-  var i=0;
-  for (i=0;i<csvArray.length; i++){
-    csvArray[i] = csvArray[i].split(",");
-  }
+function csvReader(CSVText) {
+    var csvArray = CSVText.split("\n");
+    var i = 0;
+    for (i = 0; i < csvArray.length; i++) {
+        csvArray[i] = csvArray[i].split(",");
+    }
 }
