4efd45982df1a4767aa0f42ac11b4e7cc40e1509
Nino Maruszewski <nino.maruszewski@gmail.com>
2020-09-03 21:18:59 -0500

    Switched over to PapaParse
    Now parses CSV using PapaParse.
    The button now auto-updates when you select a file.

--- src/js/csvReader.js
+++ src/js/csvReader.js
@@ -1,38 +1,18 @@
-function OpenFile(FilePath) {
-    window.open(FilePath); // Wow The worst wrapper in history
-}
+const input = document.querySelector("#CSVFile");
 
-function OpenInput() {
-    var file = document.getElementById("CSVFile").files[0];
-    console.log(file instanceof Blob);
-    //var file = filesInput.files.item(0);
-    //var file = filesInput.item(0);
-    var reader = new FileReader();
-    reader.onload = function (event) {
-        var csvArray = csvReader(event.target.result);
-        var textArray = [];
-        for (let i = 0; i < csvArray.length; i++) {
-            const element = csvArray[i];
-            textArray[i] = element.join("\t");
-        }
-        text = textArray.join("\n");
-        document.getElementById("output").innerHTML = text;
-        console.log(text);
-    };
-    reader.readAsText(file);
-}
+input.addEventListener("change", function (e) {
+    Papa.parse(input.files[0], {
+        complete: function (results) {
+            SetToFile(results);
+        },
+        header: true,
+        dynamicTyping: true,
+    });
+});
 
-function csvReader(CSVText) {
-    var csvArray = CSVText.split("\n");
-    var i = 0;
-    for (i = 0; i < csvArray.length; i++) {
-        csvArray[i] = csvArray[i].split(",");
-    }
-    return csvArray;
-}
-
-function SetToFile() {
-    document.getElementById("output").innerHTML = OpenInput(csvReader);
+function SetToFile(parsedCSV) {
+    document.getElementById("output").innerHTML = JSON.stringify(parsedCSV.data, null, 4);
+    console.log(parsedCSV.data);
 }
 
 function SetToText() {
