8ef970596d3311bb175d8939e0aed6a53e1afc49
AlemSnyder <44094752+AlemSnyder@users.noreply.github.com>
2020-09-05 09:04:56 -0500

    changes file aray to only have numbers

--- src/js/main.js
+++ src/js/main.js
@@ -3,20 +3,38 @@ const input = document.querySelector("#CSVFile");
 input.addEventListener("change", function (e) {
     Papa.parse(input.files[0], {
         complete: function (results) {
-            setToFile(results);
+            console.log(results.data);
+            try {
+            var numResults = [];
+            for (let i = 0; i < results.data.length; i++) {
+                const tableRow = results.data[i];
+                var numRow = [];
+                for (let j = 0; j < tableRow.length; j++) {
+                    const tableCell = tableRow[j];
+                    console.log(typeof (tableCell));
+                    if (typeof tableCell === 'number') { numRow[j] = tableCell; }
+                }
+                if (numRow.length === 4) { numResults.push(numRow); }
+            }
+            console.log(numResults);
+            setToFile(numResults);
+            } catch (error) {
+                document.getElementById("output").innerHTML = "Error: Make sure the csv is formatted correctly.";
+            }
         },
         dynamicTyping: true,
         worker: true,
+        skipEmptyLines: true,
     });
 });
 
 function setToFile(parsedCSV) {
     document.getElementById("output").innerHTML = JSON.stringify(
-        parsedCSV.data,
+        parsedCSV,
         null,
         4
     );
-    console.log(parsedCSV.data);
+    console.log(parsedCSV);
 }
 
 function setToText() {
